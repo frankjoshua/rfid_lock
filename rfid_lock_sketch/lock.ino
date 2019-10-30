c9387013378f28cab87cee382699a2fed50ba48a
josh <josh@tesseractmobile.com>
2019-10-29 22:01:19 -0500

    Lock HIGH LOW state is now adjustable

--- rfid_lock_sketch/lock.ino
+++ rfid_lock_sketch/lock.ino
@@ -1,19 +1,22 @@
 #define LOCK_PIN D8
 #define LOCK_INPUT 10
 
+#define LOCK_STATE LOW
+
 void initLock(){
   pinMode(LOCK_PIN, OUTPUT);
   pinMode(LOCK_INPUT, INPUT);
+  lock();
 }
 
 void unlock(){
-  digitalWrite(LOCK_PIN, HIGH);
+  digitalWrite(LOCK_PIN, LOCK_STATE);
 }
 
 void lock(){
-  digitalWrite(LOCK_PIN, LOW);
+  digitalWrite(LOCK_PIN, !LOCK_STATE);
 }
 
 boolean isLockEngaged(){
-  return digitalRead(LOCK_INPUT);
+  return digitalRead(LOCK_INPUT) == LOCK_STATE;
 }
